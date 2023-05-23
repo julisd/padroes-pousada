ffbdd6527dcd3aebda83bba767e58581a3609f63
Juliana <julisd3@gmail.com>
2023-05-23 19:12:57 -0300

    commit inicial

---
+++ src/main/java/br/edu/ifsc/pousada/Pousada/PousadaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsc.pousada.Pousada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PousadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PousadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsc/pousada/Pousada/PousadaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsc.pousada.Pousada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PousadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
